79338fbc0df0a28f23df18e69839ae4a26c70f3c
Leautrac_Vhalnery <185128974+Leautrac@users.noreply.github.com>
2024-10-23 16:35:00 +0200

    Finished 1 to 5 steps of the project, save

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-/*let prompt = window.prompt("Please choose rock, paper or scissors:").toLowerCase();
+let prompt = window.prompt("Please choose rock, paper or scissors:").toLowerCase();
 let userChoice = "";
 function getUserChoice(){
     if(prompt == "rock" || prompt == "paper" || prompt == "scissors"){
@@ -7,7 +7,7 @@ function getUserChoice(){
         return "to do shit, start again !!!";
     }
 }
-console.log(`You have chosen ${getUserChoice()}`);*/
+console.log(`You have chosen ${getUserChoice()}`);
 
 let mathRandom = Math.random();
 let computerChoice = "";
@@ -22,48 +22,45 @@ function getComputerChoice(){
         return "Issue";
     }
 }
-console.log(mathRandom);
 console.log(`The computer has chosen ${getComputerChoice()}.`);
 
-let prompt = window.prompt("Please choose rock, paper or scissors:").toLowerCase();
-let userChoice = "";
-function getUserChoice(){
-    if(prompt == "rock" || prompt == "paper" || prompt == "scissors"){
-        return userChoice = prompt;
-    } else {
-        return "to do shit, start again !!!";
+function playGame(){
+    let userScore = 0;
+    let computerScore = 0;
+    let winner = "";
+    function playRound(userChoice, computerChoice){
+        if (userChoice == computerChoice){
+            return "Draw, play again";
+        }else if (userChoice == "rock"){
+            if(computerChoice == "paper"){
+                computerScore ++;
+                winner = "computer";
+            }else {
+                userScore ++;
+                winner = "user";
+            }
+        }else if (userChoice == "paper"){
+            if (computerChoice == "scissors"){
+                computerScore ++;
+                winner = "computer";
+            }else {
+                userScore ++;
+                winner = "user";
+            }
+        }else if (userChoice == "scissors"){
+            if (computerChoice == "rock"){
+                computerScore ++;
+                winner = "computer";
+            }else {
+                userScore ++;
+                winner = "user";
+            }
+        }else {
+            return "issue";
+        }
     }
-}
-console.log(`You have chosen ${getUserChoice()}`);
-
-let userScore = 0;
-let computerScore = 0;
-
-function playRound(userChoice, computerChoice){
-    if (userChoice == computerChoice){
-        return "Draw, play again";
-    }else if (userChoice == "rock"){
-        if(computerChoice == "paper"){
-            return "You lost, paper beats rock :( !";
-        }else {
-            return "You won, rock beats scissors :) !";
-        }
-    }else if (userChoice == "paper"){
-        if (computerChoice == "scissors"){
-            return "You lost, scissors beats paper :( !";
-        }else {
-            return "You won, paper beats rock :) !";
-        }
-    }else if (userChoice == "scissors"){
-        if (computerChoice == "rock"){
-            return "You lost, rock beats scissors :( !";
-        }else {
-            return "You won, scissors beats paper :) !";
-        }
-    }else {
-        return "issue";
-    }
-}
 const userSelection = getUserChoice();
 const computerSelection = getComputerChoice();
 console.log(playRound(userSelection, computerSelection));
+console.log(`Your socre is : ${userScore}, the computer score is: ${computerScore}. The winner is ${winner} !!!`);
+}
